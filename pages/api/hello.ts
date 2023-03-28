4ec9aebc5bf47cf4cc0045bfed4dd7b4e1d003de
조정훈 <jeongh1021@naver.com>
2023-03-28 11:09:12 +0900

    트위터 모델 정의

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -8,7 +8,8 @@ export default async function handler(
   await client.user.create({
     data: {
       email: "hi",
-      name: "hi",
+      username: "hi",
+      password: "hi",
     },
   });
   res.json({
