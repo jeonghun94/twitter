7bf2622d6b1442d0dabfe29584430cfc904d3f7f
조정훈 <jeongh1021@naver.com>
2023-03-29 22:03:44 +0900

    index 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,16 @@ export default function Home() {
 
       <div className="fixed bottom-0 w-full  h-[4.5rem] bg-[#1C9BEF] grid grid-cols-2 gap-24 place-content-center place-items-center">
         <div className=" w-full flex flex-col items-end">
-          <div>
+          <div className="text-white">
             <h1 className="text-2xl font-bold">최신 소식을 놓치지 마세요</h1>
             <h3>트위터를 사용하면 가장 먼저 알게 됩니다.</h3>
           </div>
         </div>
 
         <div className=" w-full flex justify-center gap-3 font-semibold">
-          <button className="px-4 py-1 rounded-2xl border">로그인</button>
+          <button className="px-4 py-1 rounded-2xl text-white border">
+            로그인
+          </button>
           <button className="px-4 py-1 rounded-2xl text-black bg-white">
             가입하기
           </button>
